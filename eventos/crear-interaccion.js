250548ba905d296d8dc62187a0b0a2c50db7fd18
Komrade FMX <62594965+KomradeFMX@users.noreply.github.com>
2023-01-02 20:27:50 +0100

    Create crear-interaccion.js

---
+++ eventos/crear-interaccion.js
@@ -0,0 +1,22 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.InteractionCreate,
+	async execute(interaction) {
+		if (!interaction.isChatInputCommand()) return;
+
+		const command = interaction.client.commands.get(interaction.commandName);
+
+		if (!command) {
+			console.error(`No he encontrado un comando que coincida con [ ${interaction.commandName} ]`);
+			return;
+		}
+
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(`Tuve un error ejecutando ${interaction.commandName}`);
+			console.error(error);
+		}
+	},
+};
