de60a9c63e0fbfd84a88dfd7c5c598806f15d0a4
Komrade FMX <62594965+KomradeFMX@users.noreply.github.com>
2023-01-04 19:29:08 +0100

    Create purgar.js

---
+++ comandos/purgar.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('purgar')
+		.addIntegerOption(option =>
+			option.setName('mensajes').setDescription('Cantidad de mensajes que quieres borrar.').setRequired(true))
+		.setDescription('Borra mensajes en masa.')
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+	async execute(interaction) {
+
+		const cantidadMensajes = interaction.options.getInteger('mensajes');
+
+        if (cantidadMensajes > 100 || cantidadMensajes < 1) {
+            return interaction.reply({content: '¡Tienes que elegir un número entre **1** y **100**!', ephemeral: true});
+        }
+
+        await interaction.channel.bulkDelete(cantidadMensajes).catch(err => {
+            return interaction.reply({content: 'Ocurrió un error borrando los mensajes debido a limitaciones de discord :c', ephemeral: true});
+        })
+
+        await interaction.reply({content: `¡Se han borrado con éxito **${cantidadMensajes}** mensajes!`, ephemeral: true})
+	},
+};
