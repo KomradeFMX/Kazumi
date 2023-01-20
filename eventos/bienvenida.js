0365a2cf3b142657b074e5321ec11564e3767482
Komrade FMX <62594965+KomradeFMX@users.noreply.github.com>
2023-01-20 12:00:35 +0100

    Update bienvenida.js

--- eventos/bienvenida.js
+++ eventos/bienvenida.js
@@ -6,7 +6,7 @@ const { resolve } = require("path");
 module.exports = {
 	name: Events.GuildMemberAdd,
 
-	async execute(member, client) {
+	async execute(member) {
         const canalBienvenida = member.guild.systemChannel;
         //Hace el Canvas
         var bienvenido = {};
@@ -16,41 +16,50 @@ module.exports = {
         const fondo = await Canvas.loadImage('./src/img/welcum.png')
         bienvenido.contexto.drawImage(fondo, 0, 0, 1000, 600);
 
-            Canvas.registerFont(resolve("./src/fonts/UniSansHeavy.otf"), { family: "unisans" });
+            Canvas.registerFont(resolve("./src/fonts/UpheavalPro.otf"), { family: "upheaval" });
+
+            //Circulito detrás de la pfp
+            bienvenido.contexto.globalAlpha = 0.3;
+            bienvenido.contexto.beginPath();
+            bienvenido.contexto.fillStyle = '#fdb3fe';
+            bienvenido.contexto.arc(500, 203, 180, 0, Math.PI * 2, true);
+            bienvenido.contexto.fill();
+            bienvenido.contexto.closePath();
+            bienvenido.contexto.globalAlpha = 1;
 
             //Recortar un circulo para la pfp
             bienvenido.contexto.beginPath();
-            bienvenido.contexto.arc(500, 208, 165, 0, Math.PI * 2, true);
+            bienvenido.contexto.arc(500, 203, 165, 0, Math.PI * 2, true);
             bienvenido.contexto.closePath();
             bienvenido.contexto.save();
             bienvenido.contexto.clip();
 
             //Pasar el avatar de a png
-            const avatar = member.displayAvatarURL({ extension: "png" });
+            const avatar = member.displayAvatarURL({ extension: "png", size: 1024 });
 
             let cargaravatar = await Canvas.loadImage(avatar);
 
             //Dibujar la foto de perfil
-            bienvenido.contexto.drawImage(cargaravatar, 335, 43, 330, 330);
+            bienvenido.contexto.drawImage(cargaravatar, 335, 40, 330, 330);
             bienvenido.contexto.restore();
 
 
             //Nombre del usuario
-            bienvenido.contexto.font = 'bold 72px unisans';
+            bienvenido.contexto.font = 'bold 90px upheaval';
             bienvenido.contexto.textAlign = 'center';
             bienvenido.contexto.fillStyle = '#ffffff';
 
-            bienvenido.contexto.fillText(member.user.username, 500, 560);
+            bienvenido.contexto.fillText(member.user.username, 500, 550);
 
             //Nomnbre del servidor
-            bienvenido.contexto.font = 'bold 82px unisans';
+            bienvenido.contexto.font = 'bold 82px upheaval';
             bienvenido.contexto.textAlign = 'center';
             bienvenido.contexto.fillStyle = '#ffffff';
 
-            bienvenido.contexto.fillText(`¡Bienvenid@ a ${member.guild.name}`, 500, 462);
+            bienvenido.contexto.fillText(`¡Bienvenid@ a ${member.guild.name}!`, 500, 462);
 
             //Cargar la imagen al buffer
-            let imagenfinal = new AttachmentBuilder(bienvenido.crear.toBuffer('image/png'), { name: 'welcum.png' });
+            let imagenfinal = new AttachmentBuilder(bienvenido.crear.toBuffer('image/png'), { name: `welcum_${member.user.id}.png` });
             //Canal en el que quieres enviar los mensajes de bienvenida
             //Enviar el texto y la foto
             canalBienvenida.send({ content: `¡Bienvenid@ a ${member.guild.name}, ${member}!`, files: [imagenfinal] }); //No consigo mandar la imagen y el texto a la vez, si lo consigues haz eso
